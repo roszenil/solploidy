b558602e5f7dec88c216769b999dd506d52f759c
roszenil <roszenil@gmail.com>
2018-05-03 15:35:45 -0500

    updated file formats

--- summarystats/solanaceaesummarystats (Autosaved).R
+++ summarystats/solanaceaesummarystats (Autosaved).R
@@ -92,13 +92,15 @@ a<-hist(ploidy.dataset$mode.ploidy)
 
 write.tree(ploidy.tree,file="~/Dropbox/solploidy/basicdata/ploidy.tre")
 write.nexus(ploidy.tree, file="~/Dropbox/solploidy/basicdata/ploidy.nex")
-tip.names<-ploidy.tree$tip.labels
+tip.names<-ploidy.tree$tip.label
 write.table(ploidy.dataset$mode.ploidy,file="~/Dropbox/solploidy/basicdata/ploidy.txt",sep=',',row.names=FALSE, col.names=TRUE)
 aux1<-which(ploidy.dataset$mode.ploidy==2)#360 diploids
 aux<-which(ploidy.dataset$mode.ploidy!=2)#63 polyploids 14.8% of the sample
 binary.var<-rep(1,423) 
 binary.var[aux1]=0
-write.table(binary.var,file="~/Dropbox/solploidy/basicdata/binaryploidy.txt",sep=',',row.names=FALSE, col.names=TRUE)
+binary.var<-data.frame(binary.var)
+binary.table<-cbind(tip.names, binary.var)
+write.table(binary.table,file="~/Dropbox/solploidy/basicdata/binaryploidy.tsv",sep="\t",row.names=FALSE, col.names=FALSE)
 
 ###########
 
